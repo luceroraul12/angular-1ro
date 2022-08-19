6472a804f2a6a0e59ccae70a5f964970299f6039
Homitowen <pycgames@gmail.com>
2022-08-19 00:41:45 -0300

    ejercicio con ngfor y ngif anidado. nunca juntos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimeroComponent } from './components/primero/primero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeroComponent
   ],
   imports: [
     BrowserModule
