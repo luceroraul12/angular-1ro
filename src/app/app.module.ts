237170440b6c025a2bb463b231b36865db8efae0
Homitowen <pycgames@gmail.com>
2022-08-19 23:58:44 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PrimeroComponent } from './components/primero/primero.component';
+import { SegundoComponent } from './components/segundo/segundo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeroComponent
+    PrimeroComponent,
+    SegundoComponent
   ],
   imports: [
     BrowserModule
