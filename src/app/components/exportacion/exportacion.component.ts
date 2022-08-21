627432d96b79c52f7365abeeb6b0a2101b143406
Homitowen <pycgames@gmail.com>
2022-08-20 21:16:22 -0300

    exportacion-v1

--- src/app/components/exportacion/exportacion.component.ts
+++ src/app/components/exportacion/exportacion.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-exportacion',
+  selector: 'exportacion',
   templateUrl: './exportacion.component.html',
   styleUrls: ['./exportacion.component.css']
 })
